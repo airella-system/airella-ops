0633af05290933bc113e65994c388d50fe02aaec
jakubdybczak <jakub.dybczak@gmail.com>
2020-04-16 13:48:45 +0200

    Add build parameters

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env sh
+
+frontend_branch="master"
+backend_branch="master"
+
+while [[ "$#" -gt 0 ]]; do case $1 in
+  -f|--frontend-branch) frontend_branch="$2"; shift;;
+  -b|--backend-branch) backend_branch="$2"; shift;;
+  *) echo "Unknown parameter passed: $1"; exit 1;;
+esac; shift; done
+
 sh stop.sh
 cd webapp
-sh fetch.sh
+sh fetch.sh --frontend-branch $frontend_branch --backend-branch $backend_branch
 docker-compose build
--- webapp/fetch.sh
+++ webapp/fetch.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env sh
+
+frontend_branch="master"
+backend_branch="master"
+
+while [[ "$#" -gt 0 ]]; do case $1 in
+  -f|--frontend-branch) frontend_branch="$2"; shift;;
+  -b|--backend-branch) backend_branch="$2"; shift;;
+  *) echo "Unknown parameter passed: $1"; exit 1;;
+esac; shift; done
+
 rm -rf build
 mkdir build
 cd build
-git clone https://github.com/airella-system/airella-backend
-git clone https://github.com/airella-system/airella-frontend
+git clone --single-branch --branch $backend_branch https://github.com/airella-system/airella-backend
+git clone --single-branch --branch $frontend_branch https://github.com/airella-system/airella-frontend
